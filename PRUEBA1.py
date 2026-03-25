f880db46dda60becb782915fd371bf8ee98b0d23
Ivan-ht9 <iherrera@institutmvm.cat>
2026-03-25 19:21:17 +0000

    POSSIBLE ENTREGA

--- PRUEBA1.py
+++ PRUEBA1.py
@@ -1,20 +1,20 @@
 import random
-
 def obtenir_nom():
     # Llista de noms incorrectes
-    noms = ["Timotei", "Timonel", "Timbaler", "Tennebaum", "TaoPaiPai", "Teruel", "Tirolés", "Traginer", "Tourmalet"]
+    noms = "Ivan"
 
     # Llista de cognoms incorrectes
-    cognoms = ["Chandalet", "Camembert", "Sabadell", "Chevrolet", "Caganer", "Bechamel", "Casteller", "Churumbel", "Cafeaulait", "Crivillé", "Charmander"]
-
-    nom_aleatori = random.choice ([noms,cognoms]) #Sirve para crear un nombre aleatorio gracias a la funcion random.choice.
+    cognoms = "Herrera"
     
-    return (nom_aleatori)
+    noms = random.choice ([noms]) #Sirve para crear un nombre aleatorio gracias a la funcion random.choice.
+    cognoms = random.choice ([cognoms]) 
+    nom_aleatori_complet = noms + cognoms
+    return (nom_aleatori_complet)
+
     # Aquí has de construir un nom amb un nomb aleatori i un cognom aleatori de les llistes
     # retornar el nom construït
 
 def afegir_nom(llista):
-    print("PENDENT: afegir_nom")
     nom_aleatori = obtenir_nom ()
     llista = []
     llista.append(nom_aleatori)
@@ -22,10 +22,13 @@ def afegir_nom(llista):
     # Hem d'obtenir un nom aleatori, afegir-lo a la llista i mostrar per pantalla que hem afegit aquest nom
 
 def llistar_noms(llista):
-    print("PENDENT: llistar_noms")
     for nombre in llista:
         print (nombre)
     # Hem de mostrar per pantalla tots els noms que hem afegit a la llista
-obtenir_nom ()
-afegir_nom (obtenir_nom())
+
+def ordenar_noms(llista):
+    llista.sort()
+    return llista
+
 llistar_noms (afegir_nom(obtenir_nom))
+ordenar_noms (llistar_noms(afegir_nom(obtenir_nom)))
--- examen.py
+++ examen.py
@@ -5,33 +5,34 @@ import random
 
 def obtenir_nom():
     # Llista de noms incorrectes
-    noms = ["Timotei", "Timonel", "Timbaler", "Tennebaum", "TaoPaiPai", "Teruel", "Tirolés", "Traginer", "Tourmalet"]
+    noms = "Ivan"
 
     # Llista de cognoms incorrectes
-    cognoms = ["Chandalet", "Camembert", "Sabadell", "Chevrolet", "Caganer", "Bechamel", "Casteller", "Churumbel", "Cafeaulait", "Crivillé", "Charmander"]
+    cognoms = "Herrera"
+    
+    noms = random.choice ([noms]) #Sirve para crear un nombre aleatorio gracias a la funcion random.choice.
+    cognoms = random.choice ([cognoms])  #Sirve para crear un nombre aleatorio gracias a la funcion random.choice.
+    nom_aleatori_complet = noms + cognoms #Creamos el nombre completo sumando los dos valores
+    return (nom_aleatori_complet) #Devolvemos el nombre completo random
 
-    nom_aleatori = random.choice ([noms,cognoms]) #Sirve para crear un nombre aleatorio gracias a la funcion random.choice.
-    return (nom_aleatori)
     # Aquí has de construir un nom amb un nomb aleatori i un cognom aleatori de les llistes
     # retornar el nom construït
 
-def afegir_nom(llista):
-    print("PENDENT: afegir_nom")
-    nom_aleatori = obtenir_nom ()
-    llista = []
-    llista.append(nom_aleatori)
-    return llista 
+def afegir_nom(llista): #ESTA HECHO
+    nom_aleatori = obtenir_nom () #Cogemos el nombre aleatoria de la funcion obtenir nom ()
+    llista = [] #Creamos una lista vacia
+    llista.append(nom_aleatori) #Añadimos el nombre aleatoria a la lista
+    return llista #Devolvemos la lista
     # Hem d'obtenir un nom aleatori, afegir-lo a la llista i mostrar per pantalla que hem afegit aquest nom
 
-def llistar_noms(llista):
-    print("PENDENT: llistar_noms")
-    for nombre in llista:
-        print (nombre)
+def llistar_noms(llista): #ESTA HECHO
+    for nombre in llista: # Cogemos cada nombre que este dentro de la lista
+        print (nombre) # Dibujamos elnombtes por pantalla
     # Hem de mostrar per pantalla tots els noms que hem afegit a la llista
 
-def ordenar_noms(llista):
-    llista.sort()
-    return llista
+def ordenar_noms(llista):#ESTA HECHO
+    llista.sort() #Ordenamos los elementos de la lista en orden Ascendente 
+    return llista #Devolvemos la lista
     # Hem d'ordenar la llista de noms
     # Un cop ordenada la llista, llistem tots els noms
 
@@ -46,16 +47,16 @@ def mostrar_menu(): #ESTA HECHO
     # Hem de mostrar el menú que ens demanen a l'enunciat
 
 def demanar_opcio(): #ESTA HECHO
-    valores_permitidos = "a","l","o","f"
-    seleccion_user = input("Dime que opcion deseas seleccionar: ")
-    seleccion_user = seleccion_user.lower()
-    while seleccion_user not in valores_permitidos:
-        menu = mostrar_menu ()
-        input("Dime que opcion deseas seleccionar: ")
+    valores_permitidos = "a","l","o","f" #Aqui creamos la lista de valores permitidos
+    seleccion_user = input("Dime que opcion deseas seleccionar: ") #Le preguntamos al usuario que opcion quiere
+    seleccion_user = seleccion_user.lower() #Convertimos la seleccion del usuario a letras minusculas
+    while seleccion_user not in valores_permitidos: #Comenzamos el bucle dicendo que si no estan los valores permitidos se muetre el menu
+        menu = mostrar_menu () #Mostramos el menu
+        input("Dime que opcion deseas seleccionar: ")# Le volvemos a preguntar
     else:
-        if seleccion_user == valores_permitidos:
-            return seleccion_user
-        print ("Has selecionat l'opcio: ",seleccion_user)
+        if seleccion_user == valores_permitidos: # Si los valores son os permitidos
+            return seleccion_user # Que devuelva el valor seleccionado
+        print ("Has selecionat l'opcio: ",seleccion_user) # Mensaje de queopcion a elegido
 
     # Hem de demanar a l'usuari una de les opcions del menú
     # Si ens introdueix un valor incorrecte hem de tornar a mostrar el menú i tornar a demanar opció
@@ -63,13 +64,12 @@ def demanar_opcio(): #ESTA HECHO
     # Retornarem l'opció correcta sel.leccionada        
 
 def gestionar_opcio(opcio, llista):
-    print("PENDENT: gestionar_opcio")
-    opcion = demanar_opcio ()
-    match opcion:
+    opcio = demanar_opcio ()
+    match opcio:
         case "a":
-            afegir_nom ()
+            afegir_nom (llista)
         case "l":
-            llistar_noms ()
+            llistar_noms (llista)
         case "o":
             ordenar_noms (llista)
         case "f":
@@ -85,9 +85,8 @@ def gestionar_opcio(opcio, llista):
 
 print("PENDENT: programa principal")
 mostrar_menu ()
-demanar_opcio()
-gestionar_opcio ()
-ordenar_noms ()
+demanar_opcio ()
+
 
 # Heu de treballar amb una llista a la que li farem diverses operacions mostrades al menú
 # Si ens introdueixen l'opció "F" acabarem el programa
